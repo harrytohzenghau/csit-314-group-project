8478f59239b7c4aeecfbe16ae8c7ccaf5ab0b2d7
harrytohzenghau <zenghautoh@gmail.com>
2024-04-15 14:12:52 +0800

    authSlice update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,17 @@ import { RouterProvider } from "react-router-dom";
 import route from "./router/route";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { setIsLoggedIn } from "./store/authSlice";
+import { login } from "./store/authSlice";
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
+    const user = localStorage.getItem("user");
 
-    dispatch(setIsLoggedIn({ token }));
-  }, []);
+    dispatch(login({ user, token }));
+  }, [dispatch]);
 
   return <RouterProvider router={route} />;
 }
--- frontend/src/store/authSlice.js
+++ frontend/src/store/authSlice.js
@@ -3,30 +3,23 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   user: null,
   token: null,
-  isLoggedIn: false,
 };
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    setIsLoggedIn(state, action) {
-      state.isLoggedIn = true;
-      state.token = action.payload.token;
-    },
     login(state, action) {
       state.user = action.payload.user;
       state.token = action.payload.token;
-      state.isLoggedIn = true;
     },
     logout(state, action) {
       state.user = null;
       state.token = null;
-      state.isLoggedIn = false;
     },
   },
 });
 
-export const { login, logout, setIsLoggedIn } = authSlice.actions;
+export const { login, logout } = authSlice.actions;
 
 export default authSlice;
