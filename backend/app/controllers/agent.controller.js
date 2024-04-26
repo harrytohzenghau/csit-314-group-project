9d3638994514a549e12c5c4401a0413e2e7ff967
daniellim922 <daniellim922@gmail.com>
2024-04-26 20:09:41 +0800

    debugged

--- backend/app/controllers/agent.controller.js
+++ backend/app/controllers/agent.controller.js
@@ -53,13 +53,13 @@ class AgentController {
 
             res.status(201).json({
                 success: true,
-                message: "All users fetched",
+                message: "Property updated",
                 allUsers,
             });
         } catch (error) {
             res.status(500).json({
                 error,
-                message: "Failed to fetch users",
+                message: "Failed to update property",
             });
         }
     }
--- backend/app/entities/property.entity.js
+++ backend/app/entities/property.entity.js
@@ -52,7 +52,7 @@ class PropertyEntity {
     async updateProperty(property_id, data) {
         await this.fetchPropertyById(property_id);
         this.property.property_propertySchema = data.property_propertySchema;
-        this.property.property_name = data.name;
+        this.property.property_name = data.property_name;
         await this.property.save();
         return;
     }
