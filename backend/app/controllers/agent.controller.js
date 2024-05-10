0a9e23c633bd89bed83f3c28c6f40cdf9b7c847c
daniellim922 <daniellim922@gmail.com>
2024-05-11 02:08:34 +0800

    uncomment code

--- backend/app/controllers/agent.controller.js
+++ backend/app/controllers/agent.controller.js
@@ -67,9 +67,9 @@ class AgentController {
 
             let property_images = [];
 
-            // for (let i = 0; i < req.files.length; i++) {
-            //     property_images.push(req.files[i].path);
-            // }
+            for (let i = 0; i < req.files.length; i++) {
+                property_images.push(req.files[i].path);
+            }
 
             const propertyData = {
                 property_propertySchema: {
